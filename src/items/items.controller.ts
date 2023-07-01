1e3e2abe21a70087d9ce1190a157018bda18878e
浦佑輔 <yusuke@urayuusukenoMacBook-Air-2.local>
2023-07-01 17:03:20 +0900

    first commit

---
+++ src/items/items.controller.ts
@@ -0,0 +1,48 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { Item } from './item.model';
+import { ItemStatus } from './item-status.enum';
+
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
+  @Get()
+  findAll(): Item[] {
+    return this.itemsService.findAll();
+  }
+
+  @Get(':id') // /items/:id
+  findById(@Param('id') id: string): Item {
+    return this.itemsService.findById(id);
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: ItemStatus.ON_SALE,
+    };
+
+    return this.itemsService.create(item);
+  }
+
+  @Patch(':id') // /items/:id
+  updateStatus(@Param('id') id: string): Item {
+    return this.itemsService.updateStatus(id);
+  }
+
+
+  @Delete(':id') // /items/:id
+  delete(@Param('id') id: string): void {
+    this.itemsService.delete(id);
+  }
+}
